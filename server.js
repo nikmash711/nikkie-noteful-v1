225cfffbc453601ffadda123d5b877abb448d122
Nikkie Mashian <nikkiemashian@gmail.com>
2018-11-12 15:47:58 -0800

    search functionality

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ app.use(express.static('public')); // serve static files
 
 //The following block of code responds to a GET request to /api/notes and returns data in JSON format.
 app.get('/api/notes', (request, response) => {
+  const searchTerm = request.query.searchTerm; // retrieve the searchTerm from the query-string on the req.query object. 
+  if(searchTerm){
+    response.json(data.filter(note=>note.content.includes(searchTerm) || note.title.includes(searchTerm)
+    ));
+    console.log(searchTerm);
+    //if the title or content includes the searchTerm, filter the data by that
+    return;
+  }
   response.json(data);
 });
+
+/**
+   * Terse solution
+   */
+// const { searchTerm } = req.query;
+// res.json(searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data);
 
 app.get('/api/notes/:id', (request, response) => {
   const note = data.find(note => note.id === Number(request.params.id)); //its a string, and we need to make it a number 
